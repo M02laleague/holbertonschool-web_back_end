9affcd82cfe5fb79a4b8980ea5f027aa158c2a47
M02laleague <9558@holbertonstudents.com>
2024-11-25 19:53:58 +0100

    salomon github trash

--- ES6_data_manipulation/.eslintrc.js
+++ ES6_data_manipulation/.eslintrc.js
@@ -1,12 +1,37 @@
-/**
- * Retrieves a list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- * @returns {{id: Number, firstName: String, location: String}[]}
- */
-export default function getListStudents() {
-	return [
-	  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-	  { id: 2, firstName: 'James', location: 'Columbia' },
-	  { id: 5, firstName: 'Serena', location: 'San Francisco' },
-	];
-  }
+module.exports = {
+	env: {
+	  browser: false,
+	  es6: true,
+	  jest: true,
+	},
+	extends: [
+	  'airbnb-base',
+	  'plugin:jest/all',
+	],
+	globals: {
+	  Atomics: 'readonly',
+	  SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+	  ecmaVersion: 2018,
+	  sourceType: 'module',
+	},
+	plugins: ['jest'],
+	rules: {
+	  'max-classes-per-file': 'off',
+	  'no-underscore-dangle': 'off',
+	  'no-console': 'off',
+	  'no-shadow': 'off',
+	  'no-restricted-syntax': [
+		'error',
+		'LabeledStatement',
+		'WithStatement',
+	  ],
+	},
+	overrides:[
+	  {
+		files: ['*.js'],
+		excludedFiles: 'babel.config.js',
+	  }
+	]
+  };
