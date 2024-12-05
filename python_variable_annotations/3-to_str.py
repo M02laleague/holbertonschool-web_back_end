5d9cf2590a0c27e383f80a9cdb11955ecb8f35a3
M02laleague <9558@holbertonstudents.com>
2024-12-05 16:06:36 +0100

    Fixing pycodestyle

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -5,6 +5,6 @@ Task 3's module.
 
 
 def to_str(n: float) -> str:
-    '''converts a floating-point number to a string. 
+    '''Converts a floating-point number to a string.
     '''
     return str(n)
