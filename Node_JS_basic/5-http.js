896ea0ebcb2612f68e26d61d4105c71085bc1a56
M02laleague <9558@holbertonstudents.com>
2025-01-23 23:38:24 +0100

    Try 4

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,21 +1,41 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
   if (req.url === '/') {
-    res.write('Hello Holberton School!');
-    res.end();
-  } else if (req.url.startsWith('/students')) {
-    countStudents('./database.csv')
-      .then(() => res.end())
-      .catch((err) => {
-        res.statusCode = 500;
-        res.write(err.message);
-        res.end();
-      });
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    try {
+      const database = process.argv[2];
+      if (!database) {
+        throw new Error('Database file not provided');
+      }
+
+      const response = 'This is the list of our students\n';
+      await countStudents(database)
+        .then(() => {
+          res.statusCode = 200;
+          res.end(response);
+        })
+        .catch((error) => {
+          res.statusCode = 500;
+          res.end(`${response}${error.message}`);
+        });
+    } catch (error) {
+      res.statusCode = 500;
+      res.end(`This is the list of our students\n${error.message}`);
+    }
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
   }
 });
 
-app.listen(1245);
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
 
 module.exports = app;
