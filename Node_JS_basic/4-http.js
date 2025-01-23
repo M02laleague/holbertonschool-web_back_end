a46c1c1f7b6ceb85882d6914dce08c831583de32
M02laleague <9558@holbertonstudents.com>
2025-01-23 22:14:05 +0100

    Delete Node_JS_basic directory

--- Node_JS_basic/4-http.js
+++
@@ -1,9 +0,0 @@
-import http from 'http';
-
-const app = http.createServer((_req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
-});
-
-export default app;
