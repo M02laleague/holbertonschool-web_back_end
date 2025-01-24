bfd70652cd5b2f98659e869b7f0e58a268280f28
M02laleague <9558@holbertonstudents.com>
2025-01-24 22:09:05 +0100

    will do my best to fix

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -2,6 +2,7 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
@@ -9,13 +10,13 @@ app.get('/', (req, res) => {
 
 app.get('/students', async (req, res) => {
   try {
-    await countStudents(process.argv[2]);
-    res.end();
-  } catch (err) {
-    res.status(500).send(err.message);
+    const output = await countStudents(process.argv[2]);
+    res.send(`This is the list of our students\n${output}`);
+  } catch (error) {
+    res.send(`This is the list of our students\n${error.message}`);
   }
 });
 
-app.listen(1245);
+app.listen(port);
 
 module.exports = app;
